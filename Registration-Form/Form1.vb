f1f592d95076669f1313279c2277715a11c439de
ShyLow <102476483+tongaks@users.noreply.github.com>
2025-09-20 18:21:49 +0800

    Add files via upload
    initial upload

---
+++ Registration-Form/Form1.vb
@@ -0,0 +1,257 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+Imports Windows.Win32.System
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+
+
+Public Class RegistrationForm
+    Dim ConnectionString As String
+    Dim CurrentUser As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim CurrentPath As String = Directory.GetCurrentDirectory()
+        Dim DirInfo As New DirectoryInfo(CurrentPath)
+
+        ' move 4 up para tumutok sa parent dir ng project
+        For i As Integer = 1 To 4
+            If DirInfo.Parent IsNot Nothing Then
+                DirInfo = DirInfo.Parent
+            Else
+                Exit For
+            End If
+        Next
+
+        'ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & DirInfo.FullName & "\MSAccess\OOPact.accdb ;Persist Security Info=False;"
+        ConnectionString = "server=localhost;user=root;database=registrationform;port=3306;password=washer22456;"
+        LoadMunicipalityDB()
+    End Sub
+
+    Private Sub ClearAllTextBox()
+        FirstNameTxtBox.Clear()
+        MiddleNameTxtBox.Clear()
+        SurnameTxtBox.Clear()
+        SuffixCmbBox.Text = ""
+        SexCmbBox.Text = ""
+        AddressTxtBox.Clear()
+        MunicipalityCmbBox.Text = ""
+        PostalCodeTxtBox.Clear()
+        ContactNoTxtBox.Clear()
+        EmailTxtBox.Clear()
+    End Sub
+
+
+    ' crud Functions
+    Private Sub AddRecord(ByVal lname As String, ByVal fname As String, ByVal mname As String, ByVal suffix As String, ByVal sex As String, ByVal contactno As String, ByVal emailaddr As String, ByVal birthdate As String, ByVal address As String, ByVal municipality As String, ByVal postalCode As String)
+        Dim conn As MySql.Data.MySqlClient.MySqlConnection
+        Dim cmd As MySql.Data.MySqlClient.MySqlCommand
+
+        Try
+            conn = New MySql.Data.MySqlClient.MySqlConnection(ConnectionString)
+            conn.Open()
+
+            Dim sql As String = "INSERT INTO `registrationform`.`registration_form` (LastName, FirstName, MiddleName, Suffix, Sex, ContactNo, EmailAddress, BirthDate, AddressIn, Municipality, PostalCode) VALUES (@lname, @fname, @mname, @suffix, @sex, @contactno, @email, @birthdate, @addr, @muni, @postc)"
+
+            cmd = New MySql.Data.MySqlClient.MySqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("@lname", lname)
+            cmd.Parameters.AddWithValue("@fname", fname)
+            cmd.Parameters.AddWithValue("@mname", mname)
+            cmd.Parameters.AddWithValue("@suffix", suffix)
+            cmd.Parameters.AddWithValue("@sex", sex)
+            cmd.Parameters.AddWithValue("@contactno", contactno)
+            cmd.Parameters.AddWithValue("@email", emailaddr)
+            cmd.Parameters.AddWithValue("@birthdate", birthdate)
+            cmd.Parameters.AddWithValue("@addr", address)
+            cmd.Parameters.AddWithValue("@muni", municipality)
+            cmd.Parameters.AddWithValue("@postc", postalCode)
+
+            Dim result As Integer = cmd.ExecuteNonQuery()
+
+            If result > 0 Then
+                MsgBox("Record Added Successfully!", MsgBoxStyle.Information, "Success")
+            Else
+                MessageBox.Show("Failed to Add Record.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            If conn IsNot Nothing AndAlso conn.State = System.Data.ConnectionState.Open Then
+                conn.Close()
+            End If
+        End Try
+    End Sub
+
+    Private Sub LoadMunicipalityDB()
+        Dim conn As New MySqlConnection(ConnectionString)
+        Dim cmd As MySqlCommand
+        Dim reader As MySqlDataReader
+
+        Try
+            conn.Open()
+            Dim sql = "SELECT municipality_name FROM table_municipality ORDER BY municipality_name ASC"
+            cmd = New MySqlCommand(sql, conn)
+            reader = cmd.ExecuteReader
+
+            While reader.Read()
+                Dim municipality As String = If(IsDBNull(reader("municipality_name")), "NULL", reader("municipality_name"))
+                MunicipalityCmbBox.Items.Add(municipality)
+            End While
+
+        Catch ex As Exception
+            MsgBox("Error connecting to database: " & ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub LetterOnly_KeyPress(sender As Object, e As KeyPressEventArgs) Handles FirstNameTxtBox.KeyPress, SurnameTxtBox.KeyPress, MiddleNameTxtBox.KeyPress
+        If Char.IsDigit(e.KeyChar) And Not Asc(e.KeyChar) = 8 Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub NumberOnly_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ContactNoTxtBox.KeyPress, PostalCodeTxtBox.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Asc(e.KeyChar) = 8 Then
+            e.Handled = True
+        End If
+    End Sub
+
+
+
+    Private Sub ContactNoLimit(sender As Object, e As EventArgs) Handles ContactNoTxtBox.TextChanged
+        If ContactNoTxtBox.TextLength >= 11 Then
+            ContactNoTxtBox.Text = ContactNoTxtBox.Text.Remove(ContactNoTxtBox.TextLength - 1)
+            ContactNoTxtBox.Select(ContactNoTxtBox.TextLength, 1)
+        End If
+    End Sub
+
+
+
+    Private Sub RegisterBtn_Click(sender As Object, e As EventArgs) Handles RegisterBtn.Click
+        If Not ValidateRequiredFields() Then
+            Exit Sub
+        End If
+
+        Dim firstName = FirstNameTxtBox.Text
+        Dim middleName = If((MiddleNameTxtBox.Text = ""), "N/A", MiddleNameTxtBox.Text)
+        Dim surName = SurnameTxtBox.Text
+        Dim suffix = If((SuffixCmbBox.Text = ""), "N/A", SuffixCmbBox.Text)
+        Dim sex = SexCmbBox.Text
+
+        Dim contactNo = ContactNoTxtBox.Text
+        Dim emailAddr = If((EmailTxtBox.Text = ""), "N/A", EmailTxtBox.Text)
+
+        Dim birthDate = DateTimePicker1.Value
+        Dim birthDateFix = birthDate.ToString("M/d/yyyy")
+
+        Dim address = AddressTxtBox.Text
+        Dim municipality = MunicipalityCmbBox.Text
+        Dim postalCode = If((PostalCodeTxtBox.Text = ""), "N/A", PostalCodeTxtBox.Text)
+
+        AddRecord(surName, firstName, middleName, suffix, sex, contactNo, emailAddr, birthDateFix, address, municipality, postalCode)
+        ClearAllTextBox()
+    End Sub
+
+    Private Sub ConfirmationChckBox_KeyDown(sender As Object, e As KeyEventArgs) Handles ConfirmationChckBox.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If ConfirmationChckBox.Checked = False Then
+                ConfirmationChckBox.Checked = True
+            Else
+                ConfirmationChckBox.Checked = False
+            End If
+        End If
+    End Sub
+
+    Private Function ValidateRequiredFields() As Boolean
+
+        If String.IsNullOrWhiteSpace(FirstNameTxtBox.Text) Then
+            MsgBox("Name is required.", MsgBoxStyle.Critical, "Notice")
+            FirstNameTxtBox.Focus()
+            Return False
+        End If
+
+        If String.IsNullOrWhiteSpace(SurnameTxtBox.Text) Then
+            MsgBox("Surname is required.", MsgBoxStyle.Critical, "Notice")
+            SurnameTxtBox.Focus()
+            Return False
+        End If
+
+        If (String.IsNullOrWhiteSpace(SexCmbBox.Text)) Then
+            MsgBox("Sex orienation is required.", MsgBoxStyle.Critical, "Notice")
+            SexCmbBox.Focus()
+            Return False
+        End If
+
+        If ((Integer.Parse(Date.Now.ToString("yyyy"))) - Integer.Parse(DateTimePicker1.Value.Year.ToString)) < 18 Then
+            MsgBox("Age must be greater than 18 years old.", MsgBoxStyle.Critical, "Notice")
+            Return False
+        End If
+
+        If ContactNoTxtBox.Text = "" Then
+            MsgBox("Contact No. is required.", MsgBoxStyle.Critical, "Notice")
+            ContactNoTxtBox.Focus()
+            Return False
+        ElseIf ContactNoTxtBox.TextLength < 10 Then
+            MsgBox("Contact No. is less than 10 digits.", MsgBoxStyle.Critical, "Notice")
+            ContactNoTxtBox.Focus()
+            Return False
+        ElseIf ContactNoTxtBox.Text.Chars(0) <> "9" Then
+            MsgBox("First digit must be a '9'.", MsgBoxStyle.Critical, "Notice")
+            ContactNoTxtBox.Focus()
+            Return False
+        End If
+
+        If Not String.IsNullOrWhiteSpace(EmailTxtBox.Text) Then
+            Dim atSignIndex = EmailTxtBox.Text.IndexOf("@")
+            If (atSignIndex = -1) Then
+                MsgBox("E-Mail Address is invalid.", MsgBoxStyle.Critical, "Notice")
+                EmailTxtBox.Focus()
+                Return False
+            End If
+
+            Dim lastPart As String = EmailTxtBox.Text.Substring(atSignIndex)
+            If Not lastPart.Contains(".") Then
+                MsgBox("E-Mail Address is invalid.", MsgBoxStyle.Critical, "Notice")
+                EmailTxtBox.Focus()
+                Return False
+            End If
+        End If
+
+        If String.IsNullOrWhiteSpace(AddressTxtBox.Text) Then
+            MsgBox("Present Address is required.", MsgBoxStyle.Critical, "Notice")
+            AddressTxtBox.Focus()
+            Return False
+        End If
+
+        If (String.IsNullOrWhiteSpace(MunicipalityCmbBox.Text)) Then
+            MsgBox("Municipality is required.", MsgBoxStyle.Critical, "Notice")
+            MunicipalityCmbBox.Focus()
+            Return False
+        End If
+
+        If Not ConfirmationChckBox.Checked Then
+            MsgBox("Please confirm that all information above is correct.", MsgBoxStyle.Critical, "Notice")
+            ConfirmationChckBox.Focus()
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub ViewRecordBtn_Click(sender As Object, e As EventArgs) Handles ViewRecordBtn.Click
+        Form2.Show()
+    End Sub
+
+    Private Sub SexCmbBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles SexCmbBox.KeyPress
+        e.Handled = True
+    End Sub
+
+    Private Sub TextBox_Limit(sender As Object, e As EventArgs) Handles FirstNameTxtBox.TextChanged, MiddleNameTxtBox.TextChanged, SurnameTxtBox.TextChanged, EmailTxtBox.TextChanged
+        Dim caller As TextBox = DirectCast(sender, TextBox)
+        If caller.TextLength > 255 Then
+            MsgBox("Limit reached.")
+            caller.Text = caller.Text.Remove(caller.TextLength - 1)
+            caller.Select(caller.TextLength, 1)
+        End If
+    End Sub
+
+End Class
---
+++ Registration-Form/Form2.vb
@@ -0,0 +1,381 @@
+﻿Imports System.Data.Common
+Imports System.Data.OleDb
+Imports System.IO
+Imports System.Runtime
+Imports Windows.Win32.System
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Public Class Form2
+    Dim ConnectionString As String
+    Dim CurrentUser As String
+
+    Private Sub LoadMunicipalityDB()
+        Dim conn As New MySqlConnection(ConnectionString)
+        Dim cmd As MySqlCommand
+        Dim reader As MySqlDataReader
+
+        Try
+            conn.Open()
+            Dim sql = "SELECT municipality_name FROM table_municipality ORDER BY municipality_name ASC"
+            cmd = New MySqlCommand(sql, conn)
+            reader = cmd.ExecuteReader
+
+            While reader.Read()
+                Dim municipality As String = If(IsDBNull(reader("municipality_name")), "NULL", reader("municipality_name"))
+                MunicipalityCmbBox.Items.Add(municipality)
+            End While
+
+        Catch ex As Exception
+            MsgBox("Error connecting to database: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim CurrentPath As String = Directory.GetCurrentDirectory()
+        Dim DirInfo As New DirectoryInfo(CurrentPath)
+
+        ' move 4 up para tumutok sa parent dir ng project
+        For i As Integer = 1 To 4
+            If DirInfo.Parent IsNot Nothing Then
+                DirInfo = DirInfo.Parent
+            Else
+                Exit For
+            End If
+        Next
+
+        'ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & DirInfo.FullName & "\MSAccess\OOPact.accdb ;Persist Security Info=False;"
+        ConnectionString = "server=localhost;user=root;database=registrationform;port=3306;password=washer22456;"
+
+        LoadRegistrationDB()
+        LoadMunicipalityDB()
+
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.Columns(0).Width = 20
+
+        If String.IsNullOrWhiteSpace(IdTxtBox.Text) Then
+            Panel1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub UpdateRow(rowIndex As Integer, columnName As String, newValue As String)
+        Dim sql As String = "UPDATE registration_form SET [" & columnName & "] = @newVal WHERE ID = @id"
+        Dim dbConnection As New MySqlConnection
+        dbConnection = New MySqlConnection(ConnectionString)
+        Dim cmd As MySqlCommand
+
+        Try
+            dbConnection.Open()
+            cmd = New MySqlCommand(sql, dbConnection)
+            cmd.Parameters.AddWithValue("@newVal", newValue)
+            cmd.Parameters.AddWithValue("@id", rowIndex)
+            Dim res = cmd.ExecuteNonQuery()
+
+            If res > 0 Then
+                MsgBox("Successfully saved update")
+            Else
+                MsgBox("Error on update")
+            End If
+        Catch ex As Exception
+            MsgBox("Failed to save update: " + ex.ToString)
+        End Try
+
+
+    End Sub
+
+    Private Sub LoadRegistrationDB()
+        Dim sql As String = "SELECT * FROM registration_form ORDER BY ID ASC"
+        Dim dbConnection As MySqlConnection
+        Dim dbCommand As New MySqlCommand
+        Dim dbDataTable As New DataTable
+        Dim dbDataAdapter As New MySqlDataAdapter
+
+        dbConnection = New MySqlConnection(ConnectionString)
+
+        Try
+            dbConnection.Open()
+            dbCommand.Connection = dbConnection
+            dbCommand = New MySqlCommand(sql, dbConnection)
+
+            dbDataAdapter.SelectCommand = dbCommand
+            dbDataAdapter.Fill(dbDataTable)
+
+            DataGridView1.DataSource = dbDataTable
+            dbConnection.Close()
+        Catch ex As Exception
+            MsgBox("Error connecting to database: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Dim row As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+
+        For Each cell As DataGridViewCell In row.Cells
+            If cell.ColumnIndex = 0 Then
+                IdTxtBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 1 Then   'surname
+                SurnameTxtBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 2 Then   'first name
+                FirstNameTxtBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 3 Then   'middle name
+                MiddleNameTxtBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 4 Then   'suffix
+                SuffixCmbBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 5 Then   'sex
+                SexCmbBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 6 Then   'contact no
+                ContactNoTxtBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 7 Then   'email addr
+                EmailTxtBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 8 Then   'birth date
+                DateTimePicker1.Value = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 9 Then   'address
+                AddressTxtBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 10 Then   'municipality
+                MunicipalityCmbBox.Text = cell.Value.ToString
+            ElseIf cell.ColumnIndex = 11 Then   'postal code
+                PostalCodeTxtBox.Text = cell.Value.ToString
+            End If
+        Next
+    End Sub
+
+    Private Sub IDTextbox_TextChange(sender As Object, e As EventArgs) Handles IdTxtBox.TextChanged
+        Panel1.Enabled = True
+    End Sub
+
+    Private Sub DataGridView1_CellLoseFocus(sender As Object, e As DataGridViewCellValidatingEventArgs) Handles DataGridView1.CellValidating
+        'MsgBox(e.FormattedValue)
+        Dim row As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+        Dim editedCellValue As String = e.FormattedValue.ToString
+        Dim columnName = DataGridView1.Columns(e.ColumnIndex).Name
+        'MsgBox("row index: " + e.RowIndex.ToString + " column name: " + columnName + " edited cell value: " + editedCellValue)
+        'MsgBox(DataGridView1.Rows(e.RowIndex).Cells(e.ColumnIndex).Value)
+
+        Dim prevVal = DataGridView1.Rows(e.RowIndex).Cells(e.ColumnIndex).Value
+        If Not editedCellValue = prevVal Then
+            MsgBox("not changed")
+            UpdateRow(e.RowIndex + 1, columnName, editedCellValue)
+        End If
+
+    End Sub
+
+    Private Sub UpdateBtn_Click(sender As Object, e As EventArgs) Handles UpdateBtn.Click
+
+        If ValidateRequiredFields() = False Then
+            Return
+        End If
+
+        Dim id = IdTxtBox.Text
+        Dim firstName = FirstNameTxtBox.Text
+        Dim middleName = If((MiddleNameTxtBox.Text = ""), "N/A", MiddleNameTxtBox.Text)
+        Dim surName = SurnameTxtBox.Text
+        Dim suffix = If((SuffixCmbBox.Text = ""), "N/A", SuffixCmbBox.Text)
+        Dim sex = SexCmbBox.Text
+
+        Dim contactNo = ContactNoTxtBox.Text
+        Dim emailAddr = If((EmailTxtBox.Text = ""), "N/A", EmailTxtBox.Text)
+
+        Dim birthDate = DateTimePicker1.Value
+        Dim birthDateFix As String = birthDate.ToString("M/d/yyyy")
+
+        Dim address = AddressTxtBox.Text
+        Dim municipality = MunicipalityCmbBox.Text
+        Dim postalCode = If((PostalCodeTxtBox.Text = ""), "N/A", PostalCodeTxtBox.Text)
+
+        Dim message As String = $"ID: {id}{vbCrLf}" &
+                        $"First Name: {firstName}{vbCrLf}" &
+                        $"Middle Name: {middleName}{vbCrLf}" &
+                        $"Surname: {surName}{vbCrLf}" &
+                        $"Suffix: {suffix}{vbCrLf}" &
+                        $"Sex: {sex}{vbCrLf}" &
+                        $"Contact No.: {contactNo}{vbCrLf}" &
+                        $"Email: {emailAddr}{vbCrLf}" &
+                        $"Birth Date: {birthDateFix}{vbCrLf}" &
+                        $"Address: {address}{vbCrLf}" &
+                        $"Municipality: {municipality}{vbCrLf}" &
+                        $"Postal Code: {postalCode}"
+
+        MsgBox(message, MsgBoxStyle.Information, "Client Information")
+
+
+        Dim sql As String = "UPDATE registration_form SET LastName = @lname, FirstName = @fname, MiddleName = @mname, Suffix = @suffix, Sex = @sex, ContactNo = @contactno, EmailAddress = @emailaddr, BirthDate = @dbirth, AddressIn = @addr, Municipality = @muni, PostalCode = @pcode WHERE ID = @id"
+        Dim dbConnection As New MySqlConnection
+        dbConnection = New MySqlConnection(ConnectionString)
+        Dim cmd As MySqlCommand
+
+        'MsgBox(sql)
+
+        Try
+            dbConnection.Open()
+            cmd = New MySqlCommand(sql, dbConnection)
+            cmd.Parameters.AddWithValue("@lname", surName)
+            cmd.Parameters.AddWithValue("@fname", firstName)
+            cmd.Parameters.AddWithValue("@mname", middleName)
+            cmd.Parameters.AddWithValue("@suffix", suffix)
+            cmd.Parameters.AddWithValue("@sex", sex)
+            cmd.Parameters.AddWithValue("@contactno", contactNo)
+            cmd.Parameters.AddWithValue("@emailaddr", emailAddr)
+            cmd.Parameters.AddWithValue("@dbirth", birthDateFix)
+            cmd.Parameters.AddWithValue("@addr", address)
+            cmd.Parameters.AddWithValue("@muni", municipality)
+            cmd.Parameters.AddWithValue("@pcode", postalCode)
+            cmd.Parameters.AddWithValue("@id", id)
+
+            ' ask if they want to continue
+            Dim opt = MsgBox("Are you sure? ", vbYesNoCancel, "Warning")
+            If opt = vbYes Then
+                Dim res = cmd.ExecuteNonQuery()
+                If (res > 0) Then
+                    MsgBox("Successfully saved update", MsgBoxStyle.Information, "Success")
+                    LoadRegistrationDB()
+                Else
+                    MsgBox("Error on updating the record", MsgBoxStyle.Critical, "Error")
+                End If
+            Else
+                MsgBox("Operation cancelled", MsgBoxStyle.Critical, "Error")
+            End If
+
+        Catch ex As Exception
+            MsgBox("Failed to save update: " + ex.ToString)
+        End Try
+
+        'AddRecord(surName, firstName, middleName, suffix, sex, contactNo, emailAddr, birthDateFix, address, municipality, postalCode)
+    End Sub
+
+    Private Sub DeleteBtn_Click(sender As Object, e As EventArgs) Handles DeleteBtn.Click
+        If IdTxtBox.Text = "" Then
+            Return
+        End If
+
+        Dim sql As String = "DELETE FROM registration_form WHERE ID = @id"
+        Dim dbConnection As New MySqlConnection(ConnectionString)
+        Dim cmd As MySqlCommand
+
+        Try
+            dbConnection.Open()
+            cmd = New MySqlCommand(sql, dbConnection)
+            cmd.Parameters.AddWithValue("@id", IdTxtBox.Text)
+
+            Dim opt = MsgBox("Are you sure? ", vbYesNoCancel, "Warning")
+            If opt = vbYes Then
+                Dim res = cmd.ExecuteNonQuery()
+                If (res > 0) Then
+                    MsgBox("Succesfully deleted", MsgBoxStyle.Information, "Deleted")
+                    LoadRegistrationDB()
+                Else
+                    MsgBox("Error on deleting the record", MsgBoxStyle.Critical, "Error")
+                End If
+            Else
+                MsgBox("Operation cancelled", MsgBoxStyle.Critical, "Error")
+            End If
+        Catch ex As Exception
+            MsgBox("Error connecting to database: " & ex.Message)
+        End Try
+
+        'FirstNameTxtBox.Clear()
+        'MiddleNameTxtBox.Clear()
+        Panel1.Enabled = False
+
+    End Sub
+
+    Private Function ValidateRequiredFields() As Boolean
+
+        If String.IsNullOrWhiteSpace(FirstNameTxtBox.Text) Then
+            MessageBox.Show("Name is required.")
+            FirstNameTxtBox.Focus()
+            Return False
+        End If
+
+
+        If String.IsNullOrWhiteSpace(SurnameTxtBox.Text) Then
+            MessageBox.Show("Surname is required.")
+            SurnameTxtBox.Focus()
+            Return False
+        End If
+
+        If Not String.IsNullOrWhiteSpace(ContactNoTxtBox.Text) Then
+            If Not ContactNoTxtBox.Text.Chars(0) = "9" Then
+                MessageBox.Show("Invalid phone number: Must start with 9")
+                ContactNoTxtBox.Focus()
+                Return False
+            ElseIf ContactNoTxtBox.TextLength < 9 Then
+                MessageBox.Show("Phone number less than 10.")
+                ContactNoTxtBox.Focus()
+                Return False
+            End If
+        End If
+
+        If Not String.IsNullOrWhiteSpace(EmailTxtBox.Text) And Not EmailTxtBox.Text = "N/A" Then
+            Dim atSignIndex = EmailTxtBox.Text.IndexOf("@")
+            If (atSignIndex = -1) Then
+                MessageBox.Show("E-Mail Address is invalid.")
+                EmailTxtBox.Focus()
+                Return False
+            End If
+
+            Dim lastPart As String = EmailTxtBox.Text.Substring(atSignIndex)
+            If String.IsNullOrWhiteSpace(lastPart) And Not lastPart.Contains(".") Then
+                MessageBox.Show("E-Mail Address is invalid.")
+                EmailTxtBox.Focus()
+                Return False
+            End If
+        End If
+
+        If String.IsNullOrWhiteSpace(AddressTxtBox.Text) Then
+            MessageBox.Show("Home Address is required.")
+            AddressTxtBox.Focus()
+            Return False
+        End If
+
+        If (SexCmbBox.SelectedIndex = -1) Or (String.IsNullOrWhiteSpace(SexCmbBox.Text)) Then
+            MessageBox.Show("Sex is required.")
+            SexCmbBox.Focus()
+            Return False
+        End If
+
+        'If Not ConfirmationChckBox.Checked Then
+        'MsgBox("Please confirm that all information above is correct.")
+        'ConfirmationChckBox.Focus()
+        'Return False
+        'End If
+
+        If String.IsNullOrWhiteSpace(MunicipalityCmbBox.Text) Then
+            MessageBox.Show("Municipality is required.")
+            MunicipalityCmbBox.Focus()
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub SexOrientation_Limit(sender As Object, e As KeyPressEventArgs) Handles SexCmbBox.KeyPress
+        e.Handled = True
+    End Sub
+
+    Private Sub Textbox_Limit(sender As Object, e As EventArgs) Handles FirstNameTxtBox.TextChanged, SurnameTxtBox.TextChanged, MiddleNameTxtBox.TextChanged
+        Dim caller As TextBox = DirectCast(sender, TextBox)
+        If caller.TextLength > 255 Then
+            MsgBox("Limit reached")
+            caller.Text = caller.Text.Remove(caller.TextLength - 1)
+            caller.Select(caller.TextLength, 1)
+        End If
+    End Sub
+
+    Private Sub LetterOnly_KeyPress(sender As Object, e As KeyPressEventArgs) Handles FirstNameTxtBox.KeyPress, SurnameTxtBox.KeyPress, MiddleNameTxtBox.KeyPress
+        If Char.IsDigit(e.KeyChar) And Not Asc(e.KeyChar) = 8 Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub NumberOnly_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ContactNoTxtBox.KeyPress, PostalCodeTxtBox.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Asc(e.KeyChar) = 8 Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub ContactNoLimit(sender As Object, e As EventArgs) Handles ContactNoTxtBox.TextChanged
+        If ContactNoTxtBox.TextLength >= 10 Then
+            ContactNoTxtBox.Text = ContactNoTxtBox.Text.Remove(ContactNoTxtBox.TextLength - 1)
+            ContactNoTxtBox.Select(ContactNoTxtBox.TextLength, 1)
+        End If
+    End Sub
+End Class
